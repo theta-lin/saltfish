7cdbc2b3468c428956de8f54ca306fa27db1a801
theta <theta.lin@yandex.com>
2020-02-02 00:14:40 +0800

    Reimplement Editor, basic structure only, untested

--- src/editor.cpp
+++ src/editor.cpp
@@ -1,41 +1,124 @@
 #include "editor.hpp"
 
-void Editor::update(sw::Surface &surface)
+Editor::Tool::Tool(Editor &editor) : editor{editor}
 {
-	surface.fillRect(nullptr, {0, 0, 0, 255});
-	for (const Level::Line &line : level.getLines())
+}
+
+Editor::Tool::~Tool()
+{
+}
+
+Editor::NullTool::NullTool(Editor &editor) : Tool{editor}, confirmQuit{false}
+{
+	editor.status = defaultStatus + defaultStatusPrompt;
+}
+
+std::pair<bool, std::unique_ptr<Editor::Tool> > Editor::NullTool::handleEvent(const SDL_Event &event, const Vec2d &coord)
+{
+	switch (event.type)
 	{
-		LineShape lineDraw{level.getVertices()[line.v0], level.getVertices()[line.v1]};
-		lineDraw.draw({255, 255, 255, 255}, surface);
+	case SDL_KEYDOWN:
+		switch (event.key.keysym.sym)
+		{
+		case SDLK_q:
+			confirmQuit = true;
+			editor.status = quitPromptStatus;
+			break;
+
+		case SDLK_y:
+			if (confirmQuit)
+				editor.onExit();
+			break;
+
+		case SDLK_n:
+			[[fallthrough]];
+		case SDLK_ESCAPE:
+			[[fallthrough]];
+		case SDLK_RETURN:
+			if (confirmQuit)
+			{
+				confirmQuit = false;
+				editor.status = defaultStatus;
+			}
+			break;
+
+		default:
+			break;
+		}
+		break;
+
+	case SDL_MOUSEBUTTONDOWN:
+		[[fallthrough]];
+	case SDL_MOUSEMOTION:
+		if (!confirmQuit)
+			editor.status = defaultStatus + '(' + std::to_string(coord[0]) + ',' + std::to_string(coord[1]) + ')';
+		break;
+
+	default:
+		break;
 	}
 
-	for (size_t i{0}; i < level.getVertices().size(); ++i)
+	return {false, nullptr};
+}
+
+void Editor::NullTool::activate()
+{
+}
+
+void Editor::NullTool::undo()
+{
+}
+
+Editor::History::History() : current{operations.end()}
+{
+}
+
+void Editor::History::append(std::unique_ptr<Tool> tool)
+{
+	operations.erase(current, operations.end());
+	operations.push_back(std::move(tool));
+	current = operations.end();
+}
+
+bool Editor::History::undo()
+{
+	if (current == operations.begin())
+		return false;
+	--current;
+	(*current)->undo();
+	return true;
+}
+
+bool Editor::History::redo()
+{
+	if (current == operations.end())
+		return false;
+	(*current)->activate();
+	++current;
+	return true;
+}
+
+void Editor::toolHandleEvent(const SDL_Event &event, const Vec2d &coord)
+{
+	auto ret{tool->handleEvent(event, coord)};
+	if (ret.second)
 	{
-		sw::Surface indexRender{font.renderBlended(std::to_string(i), {255, 130, 0, 255})};
-		sw::Rect dstRect{static_cast<int>(std::roundl(level.getVertices()[i][0])),
-				         static_cast<int>(std::roundl(level.getVertices()[i][1])),
-		                 0, 0};
-		indexRender.blit(surface, nullptr, &dstRect);
+		if (ret.first)
+		{
+			history.append(std::move(tool));
+		}
+		tool = std::move(ret.second);
 	}
 }
 
-Editor::Editor(Game &game, UI &ui, const std::filesystem::path &exeDir, std::function<void()> onExit)
-	: level{game.getLevel()}, ui{ui}, font{exeDir / "font" / "DejaVuSansMono.ttf", 20},
-	screen{{0.0, 0.0, 1.0, 0.9}, [this](sw::Surface &surface){ this->update(surface); }},
-	input{{0.0, 0.9, 1.0, 0.05}, {{255, 255, 255, 255}, {40, 40, 40, 255}}, exeDir / "font" / "DejaVuSansMono.ttf"},
-	message{{0.0, 0.95, 1.0, 0.05}, {{255, 130, 0, 255}, {40, 40, 40, 255}}, exeDir / "font" / "DejaVuSansMono.ttf"},
-	onExit{onExit}
+Editor::Editor(const DoubleRect &dimension, sw::Window &window, Game &game, Log &logger, std::string &status, std::string &message, std::function<void()> onExit)
+	: Widget{dimension}, view{{0.0, 0.0}, 0.005},
+	  window{window}, game{game}, tool{std::make_unique<NullTool>(*this)},
+	  hLocked{false}, yAlign{0}, vLocked{false}, xAlign{0},
+	  leftMouseButtonDown{false}, dragged{false}, xOrigin{0}, yOrigin{0},
+	  logger{logger}, status{status}, message{message}, onExit{onExit}
 {
-	ui.add(screen);
-	ui.add(input);
-	ui.add(message);
-}
 
-Editor::~Editor()
-{
-	ui.remove(screen);
-	ui.remove(input);
-	ui.remove(message);
 }
 
 void Editor::handleEvent(const SDL_Event &event)
@@ -43,114 +126,279 @@ void Editor::handleEvent(const SDL_Event &event)
 	switch (event.type)
 	{
 	case SDL_KEYDOWN:
+		switch (event.key.keysym.sym)
 		{
-			switch(event.key.keysym.scancode)
-			{
-			case SDL_SCANCODE_BACKSPACE:
-				if (input.getText().size() > 0)
-					input.getText().pop_back();
-				break;
+		case SDLK_ESCAPE:
+			tool = std::make_unique<NullTool>(*this);
+			break;
 
-			case SDL_SCANCODE_RETURN:
-			{
-				auto tokens{tokenize(input.getText())};
-				auto it = tokens.begin();
-				input.getText().clear();
-				message.getText().clear();
-				if (tokens.size() >= 1)
-				{
-					try
-					{
-						if (*it == "q" || *it == "quit")
-						{
-							onExit();
-						}
-						else if (*it == "n" || *it == "new")
-						{
-							level.free();
-						}
-						else if (*it == "o" || *it == "open")
-						{
-							std::string levelName;
-							convertTokens(++it, tokens.end(), levelName);
-							level.free();
-							if (!level.load(levelName))
-								message.getText() = "Error: level.load() failed";
-						}
-						else if (*it == "s" || *it == "save")
-						{
-							std::string levelName;
-							convertTokens(++it, tokens.end(), levelName);
-							if (!level.save(levelName))
-								message.getText() = "Error: level.save() failed";
-						}
-						else if (*it == "v" || *it == "vertex")
-						{
-							Level::Vertex vertex;
-							convertTokens(++it, tokens.end(), vertex[0], vertex[1]);
-							if (!level.addVertex(vertex))
-								message.getText() = "Error: level.addVertex() failed";
-						}
-						else if (*it == "l" || *it == "line")
-						{
-							Level::Line line;
-							convertTokens(++it, tokens.end(), line.v0, line.v1);
-							if (!level.addLine(line))
-								message.getText() = "Error: level.addLine() failed";
-						}
-						else if (*it == "d" || *it == "delete")
-						{
-							if (tokens.size() >= 2)
-							{
-								++it;
-								if (*it == "v" || *it == "vertex")
-								{
-									uint16_t index;
-									convertTokens(++it, tokens.end(), index);
-									if (!level.removeVertex(index))
-										message.getText() = "Error: level.removeVertex() failed";
-								}
-								else if (*it == "l" || *it == "line")
-								{
-									uint16_t v0, v1;
-									convertTokens(++it, tokens.end(), v0, v1);
-									if (!level.removeLine(v0, v1))
-										message.getText() = "Error: level.removeLine() failed";
-								}
-								else
-								{
-									message.getText() = "Error: delete: Command does not exist";
-								}
-							}
-							else
-							{
-								message.getText() = "Error: delete: Wrong number of tokens";
-							}
-						}
-						else
-						{
-							message.getText() = "Error: Command does not exist";
-						}
-					}
-					catch (std::runtime_error &exception)
-					{
-						message.getText() = exception.what();
-					}
-				}
-			}
+		case SDLK_u:
+			tool = std::make_unique<NullTool>(*this);
+			if (history.undo())
+				message = "Undo one operation";
+			else
+				message = "Already at oldest change";
+			break;
 
-			default:
-				break;
-			}
+		case SDLK_r:
+			tool = std::make_unique<NullTool>(*this);
+			if (history.redo())
+				message = "Redo one operation";
+			else
+				message = "Already at newest change";
+			break;
+
+		case SDLK_LSHIFT:
+			hLocked = !hLocked;
+			if (hLocked)
+				message = "Horizontal locked";
+			else
+				message = "Horizontal unlocked";
+			vLocked = false;
+			break;
+
+		case SDLK_LALT:
+			vLocked = !vLocked;
+			if (vLocked)
+				message = "Vertical locked";
+			else
+				message = "Vertical unlocked";
+			hLocked = false;
+			break;
+
+		default:
+			toolHandleEvent(event, {});
+			break;
 		}
 		break;
 
-	case SDL_TEXTINPUT:
-		input.getText() += event.text.text;
+	case SDL_MOUSEMOTION:
+	{
+		SDL_Event modified{event};
+		if (hLocked)
+		{
+			SDL_WarpMouseInWindow(window.getPtr(), event.motion.x, yAlign);
+			modified.motion.y = yAlign;
+		}
+		else
+		{
+			yAlign = event.motion.y;
+		}
+
+		if (vLocked)
+		{
+			SDL_WarpMouseInWindow(window.getPtr(), xAlign, event.motion.y);
+			modified.motion.x = xAlign;
+		}
+		else
+		{
+			xAlign = event.motion.x;
+		}
+
+		if (leftMouseButtonDown)
+		{
+			dragged = true;
+
+			// Here we are dragging the view OPPOSITE of mouse's motion,
+			// because we actually want to drag the CANVAS not the VIEW
+			view.origin[0] += (xOrigin - modified.motion.x) * view.scale;
+			view.origin[1] += (yOrigin - modified.motion.y) * view.scale;
+			message = "Dragging";
+		}
+		else
+		{
+			Vec2d coord{static_cast<double>(modified.motion.x), static_cast<double>(modified.motion.y)};
+			coord *= view.scale;
+			coord += view.origin;
+			toolHandleEvent(modified, coord);
+		}
+
+		xOrigin = modified.motion.x;
+		yOrigin = modified.motion.y;
+		break;
+	}
+
+	case SDL_MOUSEBUTTONDOWN:
+		if (event.button.button == SDL_BUTTON_LEFT)
+			leftMouseButtonDown = true;
 		break;
 
-	default:
+	case SDL_MOUSEBUTTONUP:
+		if (event.button.button == SDL_BUTTON_LEFT)
+		{
+			if (!dragged)
+			{
+				Vec2d coord{static_cast<double>(event.button.x), static_cast<double>(event.button.y)};
+				coord *= view.scale;
+				coord += view.origin;
+				toolHandleEvent(event, coord);
+			}
+			leftMouseButtonDown = false;
+			dragged = false;
+		}
 		break;
+
+	case SDL_MOUSEWHEEL:
+	{
+		double zoom{zoomCoeff * event.wheel.y};
+		if (event.wheel.direction == SDL_MOUSEWHEEL_FLIPPED)
+			zoom = -zoom;
+		view.scale = std::clamp(view.scale + zoom, zoomMin, zoomMax);
+		break;
+	}
 	}
 }
 
+void Editor::draw(sw::Surface &surface)
+{
+	surface.fillRect(&real, backgroundColor);
+	auto vertices{game.level.getVertices()};
+	for (const Level::Line &line : game.level.getLines())
+	{
+		LineShape lineDraw{
+		                  	(vertices[line.v0] - view.origin) / view.scale,
+							(vertices[line.v1] - view.origin) / view.scale
+		                  };
+		lineDraw.draw(foregroundColor, surface);
+	}
+}
+
+/*
+ *void Editor::Canvas::onDraw(sw::Surface &surface)
+ *{
+ *    surface.fillRect(nullptr, {0, 0, 0, 255});
+ *    for (const Level::Line &line : level.getLines())
+ *    {
+ *        LineShape lineDraw{level.getVertices()[line.v0], level.getVertices()[line.v1]};
+ *        lineDraw.draw({255, 255, 255, 255}, surface);
+ *    }
+ *
+ *    for (size_t i{0}; i < level.getVertices().size(); ++i)
+ *    {
+ *        sw::Surface indexRender{font.renderBlended(std::to_string(i), {255, 130, 0, 255})};
+ *        sw::Rect dstRect{static_cast<int>(std::roundl(level.getVertices()[i][0])),
+ *                         static_cast<int>(std::roundl(level.getVertices()[i][1])),
+ *                         0, 0};
+ *        indexRender.blit(surface, nullptr, &dstRect);
+ *    }
+ *}
+ */
+
+/*
+ *void Editor::handleEvent(const SDL_Event &event)
+ *{
+ *    switch (event.type)
+ *    {
+ *    case SDL_KEYDOWN:
+ *        {
+ *            switch(event.key.keysym.scancode)
+ *            {
+ *            case SDL_SCANCODE_BACKSPACE:
+ *                if (input.getText().size() > 0)
+ *                    input.getText().pop_back();
+ *                break;
+ *
+ *            case SDL_SCANCODE_RETURN:
+ *            {
+ *                auto tokens{tokenize(input.getText())};
+ *                auto it = tokens.begin();
+ *                input.getText().clear();
+ *                message.getText().clear();
+ *                if (tokens.size() >= 1)
+ *                {
+ *                    try
+ *                    {
+ *                        if (*it == "q" || *it == "quit")
+ *                        {
+ *                            onExit();
+ *                        }
+ *                        else if (*it == "n" || *it == "new")
+ *                        {
+ *                            level.free();
+ *                        }
+ *                        else if (*it == "o" || *it == "open")
+ *                        {
+ *                            std::string levelName;
+ *                            convertTokens(++it, tokens.end(), levelName);
+ *                            level.free();
+ *                            if (!level.load(levelName))
+ *                                message.getText() = "Error: level.load() failed";
+ *                        }
+ *                        else if (*it == "s" || *it == "save")
+ *                        {
+ *                            std::string levelName;
+ *                            convertTokens(++it, tokens.end(), levelName);
+ *                            if (!level.save(levelName))
+ *                                message.getText() = "Error: level.save() failed";
+ *                        }
+ *                        else if (*it == "v" || *it == "vertex")
+ *                        {
+ *                            Level::Vertex vertex;
+ *                            convertTokens(++it, tokens.end(), vertex[0], vertex[1]);
+ *                            if (!level.addVertex(vertex))
+ *                                message.getText() = "Error: level.addVertex() failed";
+ *                        }
+ *                        else if (*it == "l" || *it == "line")
+ *                        {
+ *                            Level::Line line;
+ *                            convertTokens(++it, tokens.end(), line.v0, line.v1);
+ *                            if (!level.addLine(line))
+ *                                message.getText() = "Error: level.addLine() failed";
+ *                        }
+ *                        else if (*it == "d" || *it == "delete")
+ *                        {
+ *                            if (tokens.size() >= 2)
+ *                            {
+ *                                ++it;
+ *                                if (*it == "v" || *it == "vertex")
+ *                                {
+ *                                    uint16_t index;
+ *                                    convertTokens(++it, tokens.end(), index);
+ *                                    if (!level.removeVertex(index))
+ *                                        message.getText() = "Error: level.removeVertex() failed";
+ *                                }
+ *                                else if (*it == "l" || *it == "line")
+ *                                {
+ *                                    uint16_t v0, v1;
+ *                                    convertTokens(++it, tokens.end(), v0, v1);
+ *                                    if (!level.removeLine(v0, v1))
+ *                                        message.getText() = "Error: level.removeLine() failed";
+ *                                }
+ *                                else
+ *                                {
+ *                                    message.getText() = "Error: delete: Command does not exist";
+ *                                }
+ *                            }
+ *                            else
+ *                            {
+ *                                message.getText() = "Error: delete: Wrong number of tokens";
+ *                            }
+ *                        }
+ *                        else
+ *                        {
+ *                            message.getText() = "Error: Command does not exist";
+ *                        }
+ *                    }
+ *                    catch (std::runtime_error &exception)
+ *                    {
+ *                        message.getText() = exception.what();
+ *                    }
+ *                }
+ *            }
+ *
+ *            default:
+ *                break;
+ *            }
+ *        }
+ *        break;
+ *
+ *    case SDL_TEXTINPUT:
+ *        input.getText() += event.text.text;
+ *        break;
+ *
+ *    default:
+ *        break;
+ *    }
+ *}
+ *
+ */
--- src/editor.hpp
+++ src/editor.hpp
@@ -14,19 +14,31 @@
  * NOTE: To differentiate between canvas and preview,
  *       use "editor" or "preview" in names.
  *
- * A game level editor, will be hooked on UI by EditorState of ProgramState.
- * It displays the objects on screen, and forwards all the events to Tool,
- * ALL SCREEN COORDINATES ARE TRANSLATED INTO REAL COORDINATES before doing so.
+ * A game level editor, will be hooked onto UI by EditorState of ProgramState.
+ * It displays the objects on screen, and forwards most of the events to tool,
+ * ALL SCREEN COORDINATES ARE TRANSLATED INTO REAL COORDINATES,
+ * and A LEFT MOUSE BUTTON DOWN-UP PAIR IS ONLY FORWARDED WHEN BOTH OPERATIONS HAVE THE SAME COORDINATES
+ * (otherwise considered a drag operation) before doing so.
+ * It provides the access of level, status, and message to tool;
+ * and it receives the operation to be appended to history.
+ * Tool also gives some hints to UI (TODO).
  *
- * +--------+     +-----------+             +---------+
- * |  UI    |====>|  Editor   | EditorEvent |+-------+|
- * | Events |     |+---------+|------------->| Field ||     +----------------+
- * +--------+     || History ||      UI     |+-------+|---->| Status String  |
- *                |+---------+|<------------|         |     +----------------+
- *                |+---------+|             |         |
- *                ||  Level  |------------->|  Tool   |     +----------------+
- *                |+---------+|             |         |---->| Message String |
- *                +-----------+             +---------+     +----------------+
+ *                      +-------------------------------------------+
+ *                      |                                           |
+ * +--------+     +-----------+             +---------+             v
+ * |  UI    |====>|           |   Events    |         |     +----------------+
+ * | Events |     |           |------------>|         |---->| Status String  |
+ * +--------+     |  Editor   |UI, Operation|         |     +----------------+
+ *                |           |<------------|  Tool   |
+ *                |+---------+|  Interface  |         |     +----------------+
+ *                || History ||------------>|         |---->| Message String |
+ *                |+---------+|             |         |     +----------------+
+ *                +-----------+             +---------+
+ *                      ^                        ^
+ *                      |        +-------+       |
+ *                      +--------| Level +-------+
+ *                               +-------+
+ *
  *
  * Note: It actually store a Game object instead of simply a Level,
  *       although it won't be needed now, it will be useful for the preview function in the future.
@@ -40,57 +52,67 @@ private:
 	 */
 	struct ViewRect
 	{
-		double x;
-		double y;
+		Vec2d origin;
 		double scale;
 	};
 
 	/*
-	 * The current Tool is kept similar to a state machine,
-	 * the next Tool will be returned through handleEvent().
+	 * The current tool is kept similar to a state machine,
+	 * the next tool will be returned through handleEvent().
 	 */
 	class Tool
 	{
-	private:
+	protected:
 		// Distance(proportion to screen HEIGHT) from the object that,
 		// during mouse click, will still count as selecting the object.
 		// Note: Uses screen height because screen width varies a lot.
 		const double vertexCanvasEps{0.002};
 		const double lineCanvasEps{0.002};
 
-		std::string &status;
-		std::string &message;
+		Editor &editor;
 
 	public:
-		Tool(std::string &status, std::string &message);
+		Tool(Editor &editor);
 		virtual ~Tool();
 
 		// Received SDL_Event, if containing coordinates, will be translated into real coordinates.
+		// Note: Coordinates in "event" is screen coordinates, "coord" is real coordinates
 		// Return true to append to history,
-		// and a new Tool or nullptr for no change.
-		virtual std::pair<bool, std::unique_ptr<Tool> > handleEvent(Editor &editor, const SDL_Event &event) = 0;
+		// and a new tool or nullptr for no change.
+		virtual std::pair<bool, std::unique_ptr<Tool> > handleEvent(const SDL_Event &event, const Vec2d &coord) = 0;
 
-		// Undo operation, supposed to be called from History.
+		// Activate(acting as redo if called from outside) and undo operation, supposed to be called from History.
+		// activate() can also be called internally by handleEvent()
 		// Throws EXCEPTION if failed (it is supposed to be a valid operation when appended to History).
+		virtual void activate() = 0;
 		virtual void undo() = 0;
 	};
 
 	class NullTool : public Tool
 	{
+	private:
+		const std::string defaultStatus{"NULL TOOL: "};
+		const std::string defaultStatusPrompt{"Press \'q\' to quit"};
+		const std::string quitPromptStatus{"Are you sure to quit? [y/N]"};
+		// true for waiting for the user to confirm quitting
+		bool confirmQuit;
+
 	public:
-		using Tool::Tool;
-		std::pair<bool, std::unique_ptr<Tool> > handleEvent(Editor &editor, const SDL_Event &event) override;
+		NullTool(Editor &editor);
+		std::pair<bool, std::unique_ptr<Tool> > handleEvent(const SDL_Event &event, const Vec2d &coord) override;
+		virtual void activate() override;
 		virtual void undo() override;
 	};
 
 	/*
-	 * Maintains a pointer to a list of operations in order to undo/redo by moving the pointer
+	 * Maintains a pointer to a list of operations in order to undo/redo by moving the pointer.
+	 * The current pointer points at one past the last operation.
 	 */
 	class History
 	{
 	private:
 		std::list<std::unique_ptr<Tool> > operations;
-		std::size_t current;
+		std::list<std::unique_ptr<Tool> >::iterator current;
 
 	public:
 		History();
@@ -105,26 +127,46 @@ private:
 	};
 
 	ViewRect view;
+	sw::Window &window;
 	Game &game;
 	std::unique_ptr<Tool> tool;
-	std::filesystem::path fontPath;
-	sw::Font font;
+	History history;
 
 	// Track the status of horizontal/vertical locking of mouse
-	int32_t xOrigin;
 	bool    hLocked;
-	int32_t yOrigin;
+	int32_t yAlign;
 	bool    vLocked;
+	int32_t xAlign;
 
 	// Allow dragging the map
 	bool leftMouseButtonDown;
+	bool dragged;
+	int32_t xOrigin;
+	int32_t yOrigin;
 
-	std::function<void()> onExit;
+	// change of view scale / mouse wheel scroll amount
+	const double zoomCoeff{0.0005};
+	// min and max is actually opposite (min = max zoom in)
+	const double zoomMin{0.001};
+	const double zoomMax{1.0};
 
-	void cachedDrawNumber(sw::Surface &surface, const sw::Rect &rect, int number);
+	const sw::Color foregroundColor{255, 255, 255, 255};
+	const sw::Color backgroundColor{0  ,   0,   0, 255};
+
+	// Wrapper to deal with tool pointer and history after tool handled event
+	void toolHandleEvent(const SDL_Event &event, const Vec2d &coord);
 
 public:
-	Editor(const DoubleRect &dimension, Game &game, std::string &status, std::string &message, const std::filesystem::path &fontPath, std::function<void()> onExit);
+	Log &logger;
+
+	// Display current tool and input for tool
+	std::string &status;
+	// Display input to EDITOR itself, messages, and errors
+	std::string &message;
+
+	const std::function<void()> onExit;
+
+	Editor(const DoubleRect &dimension, sw::Window &window, Game &game, Log &logger, std::string &status, std::string &message, std::function<void()> onExit);
 	void handleEvent(const SDL_Event &event);
 	void draw(sw::Surface &surface) final;
 };
